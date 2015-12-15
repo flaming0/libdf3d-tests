57a21be2e7e9bd3c7530bb1a47942ab2b70c09ce
flamingo <epiforce57@gmail.com>
2015-12-15 21:15:45 +0300

    Setting up the application.

--- code/src/TestAppDelegate.cpp
+++ code/src/TestAppDelegate.cpp
@@ -1,1 +1,31 @@
 #include "TestAppDelegate.h"
+
+df3d::EngineInitParams TestAppDelegate::getInitParams() const
+{
+    return df3d::EngineInitParams();
+}
+
+bool TestAppDelegate::onAppStarted()
+{
+    return true;
+}
+
+void TestAppDelegate::onAppEnded()
+{
+
+}
+
+void TestAppDelegate::onAppPaused()
+{
+
+}
+
+void TestAppDelegate::onAppResumed()
+{
+
+}
+
+void df3dInitialized()
+{
+    df3d::Application::setupDelegate(make_unique<TestAppDelegate>());
+}
--- code/src/TestAppDelegate.h
+++ code/src/TestAppDelegate.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "df3d.h"
+
+class TestAppDelegate : public df3d::AppDelegate
+{
+public:
+    df3d::EngineInitParams getInitParams() const override;
+
+    bool onAppStarted() override;
+    void onAppEnded() override;
+
+    void onAppPaused() override;
+    void onAppResumed() override;
+};
