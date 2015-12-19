861a086fdfd2eea43d2864fa8f9c66cf268849fa
flamingo <epiforce57@gmail.com>
2015-12-19 22:21:46 +0300

    Add __old.

---
+++ __old/game/component_serializers/ParticleSystemComponentSerializer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "ComponentSerializer.h"
+
+namespace df3d { namespace component_serializers {
+
+class ParticleSystemComponentSerializer : public ComponentSerializer
+{
+public:
+	shared_ptr<NodeComponent> fromJson(const Json::Value &root);
+    Json::Value toJson(shared_ptr<NodeComponent> component);
+};
+
+} }
