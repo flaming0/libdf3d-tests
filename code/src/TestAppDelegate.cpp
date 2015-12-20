5eaa00fec77cba87af54d10e6fc7119f8e24f1de
flamingo <epiforce57@gmail.com>
2015-12-20 20:43:13 +0300

    Fix.

--- code/src/TestAppDelegate.cpp
+++ code/src/TestAppDelegate.cpp
@@ -20,8 +20,8 @@ bool TestAppDelegate::onAppStarted()
 
     auto e1 = df3d::world().spawn();
     df3d::world().staticMesh().add(e1, "data/teapot.obj", df3d::ResourceLoadingMode::IMMEDIATE);
-    df3d::world().transform().setPosition(df3d::world().transform().lookup(e1), glm::vec3(0.0f, 0.0f, -20.0f));
-    df3d::world().transform().setScale(df3d::world().transform().lookup(e1), glm::vec3(4.0f, 4.0f, 4.0f));
+    df3d::world().transform().setPosition(e1, glm::vec3(0.0f, 0.0f, -20.0f));
+    df3d::world().transform().setScale(e1, glm::vec3(4.0f, 4.0f, 4.0f));
 
     df3d::world().setCamera(make_shared<df3d::FPSCamera>(1.0f, true));
     //df3d::svc().debugConsole()->show();
