e70e17798dc765af5c9d72b455c2e9379e2b8e8e
flamingo <epiforce57@gmail.com>
2015-12-23 22:50:41 +0300

    Update.

--- code/src/TestAppDelegate.cpp
+++ code/src/TestAppDelegate.cpp
@@ -20,13 +20,22 @@ bool TestAppDelegate::onAppStarted()
 
     RUN_ALL_TESTS();
 
-    auto e2 = df3d::world().spawn();
-    df3d::world().audio().add(e2, "data/4mjb00vZq77d.ogg");
-    df3d::world().audio().play(e2);
-
+    // Partially load from scene def file.
     df3d::replaceWorld(df3d::World::newWorld("data/test_world.world"));
     df3d::world().setCamera(make_shared<df3d::FPSCamera>(1.0f, true));
-    df3d::world().physics().getPhysicsWorld()->setGravity({ 0.0f, -1.0f, 0.0f });
+    df3d::world().physics().getPhysicsWorld()->setGravity({ 0.0f, -10.0f, 0.0f });
+
+    // Partially manually.
+    auto e = df3d::world().spawn();
+
+    df3d::world().staticMesh().add(e, "data/cube_textured.obj", df3d::ResourceLoadingMode::IMMEDIATE);
+    df3d::world().transform().translate(e, { 0.0f, -10.0, 0.0f });
+    df3d::world().transform().scale(e, { 50.0f, 0.0001f, 50.0f });
+    df3d::PhysicsComponentCreationParams params;
+    params.mass = 0.0f;     // Make it static body.
+    df3d::world().physics().add(e, params, df3d::world().staticMesh().getMeshData(e));
+
+    df3d::svc().debugConsole()->getCVars().set(df3d::CVAR_DEBUG_DRAW, "1");
 
     return true;
 }
